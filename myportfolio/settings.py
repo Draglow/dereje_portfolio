81ff53180cd7faef0723231c2ff1ba5dd5bf7aab
Dereje Amare <draglow21@gmail.com>
2024-05-19 10:56:14 +0300

    add some styling

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -139,4 +142,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'draglow21@gmail.com'  
-EMAIL_HOST_PASSWORD = 'dosn qmaw xswa oxbw'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+# Load environment variables from .env file
+
+
+# Use environment variables in your settings
